d79858297e2b279bb81ccadc8cfe0fd1476ba475
Superstar-Lucas <lucaslee971127@gmail.com>
2024-06-15 21:27:42 +0800

    upload portfolio

--- portfolio.ts
+++ portfolio.ts
@@ -12,72 +12,132 @@ import {
 } from "./types/sections";
 
 export const greetings: GreetingsType = {
-  name: "Hanzla Tauqeer",
-  title: "Hi all, I'm Hanzla",
+  name: "Lucas Lee",
+  title: "Hi all, I'm Lucas",
   description:
-    "I'm a passionate Full Stack web developer with experience developing Full Stack web applications with Python, Django, React.js, and Cloud Technologies. I am strongly interested in learning new technologies and implementing them in my projects. I'm a self-motivated and hardworking individual who is always ready to learn new things and work in a team.",
+    "Fullstack Blockchain developer specializing in Solidity and Ethereum. Crafting innovative smart contracts for DeFi, NFTs and more. Eager to build the decentralized future.",
   resumeLink: "#",
 };
 
 export const openSource = {
-  githubUserName: "1hanzla100",
+  githubUserName: "Superstar-Lucas",
 };
 
 export const contact = {};
 
 export const socialLinks: SocialLinksType = {
-  email: "mailto:hanzla.tauqeer123@gmail.com",
-  linkedin: "https://www.linkedin.com/in/1hanzla100/",
-  github: "https://github.com/1hanzla100",
-  instagram: "https://www.instagram.com/__hanzla100",
+  email: "lucaslee971127@gmail.com",
+  linkedin: "https://www.linkedin.com/in/lucas-lee971127/",
+  github: "https://github.com/Superstar-Lucas",
+  // instagram: "https://www.instagram.com/__hanzla100",
   // facebook: 'https://www.facebook.com/1hanzla100',
   // twitter: 'https://twitter.com/1hanzla100',
 };
 
 export const skillsSection: SkillsSectionType = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "CRAZY FULL STACK BLOCKCHAIN DEVELOPER WHO WANTS TO EXPLORE EVERY CRYPTO WORLD",
   data: [
     {
       title: "Full Stack Development",
       lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
       skills: [
-        emoji("⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js"),
-        emoji("⚡ Building responsive static websites using Next.js"),
-        emoji("⚡ Building RESTful APIs in Django & Django REST Framework"),
+        emoji("⚡ Crafted smart contracts and DAPPs across various EVM Blockchain platforms including Ethereum, BSC, Polygon, and Blast"),
+        emoji("⚡ Proficient in Web3 technologies and frameworks such as Web3.js, Ethers.js, and interfacing with decentralized networks and protocols."),
+        emoji("⚡ Experienced in project development within several crypto ecosystems such as DeFi, P2E, NFT, and DAO."),
       ],
       softwareSkills: [
         {
-          skillName: "Python",
-          iconifyTag: "logos:python",
+          skillName: "Solidity",
+          iconifyTag: "logos:solidity",
         },
         {
-          skillName: "Django",
-          iconifyTag: "vscode-icons:file-type-django",
-        },
-        {
-          skillName: "JavaScript",
-          iconifyTag: "logos:javascript",
-        },
-        {
-          skillName: "Reactjs",
-          iconifyTag: "vscode-icons:file-type-reactjs",
+          skillName: "Typescript",
+          iconifyTag: "nonicons:typescript-16",
         },
         {
           skillName: "Nextjs",
           iconifyTag: "vscode-icons:file-type-light-next",
         },
         {
-          skillName: "AWS",
-          iconifyTag: "logos:aws",
+          skillName: "Web3js",
+          iconifyTag: "devicon:web3",
         },
         {
-          skillName: "Heroku",
-          iconifyTag: "logos:heroku-icon",
+          skillName: "Etherjs",
+          iconifyTag: "fa6-brands:ethereum",
+        },
+        {
+          skillName: "Rust",
+          iconifyTag: "cib:rust",
+        },
+        {
+          skillName: "Golang",
+          iconifyTag: "fa6-brands:golang",
+        },
+        {
+          skillName: "Solana",
+          iconifyTag: "token-branded:solana",
+        },
+        {
+          skillName: "Hardhat",
+          iconifyTag: "devicon:hardhat",
+        },
+        {
+          skillName: "Foundry",
+          iconifyTag: "simple-icons:foundryvirtualtabletop",
+        },
+        {
+          skillName: "Ruby on Rails",
+          iconifyTag: "simple-icons:rubyonrails",
+        },
+        {
+          skillName: "Expressjs",
+          iconifyTag: "skill-icons:expressjs-light",
+        },
+        {
+          skillName: "Graphql",
+          iconifyTag: "simple-icons:graphql",
+        },
+        {
+          skillName: "PostgreSQL",
+          iconifyTag: "simple-icons:postgresql",
+        },
+        {
+          skillName: "Apollo",
+          iconifyTag: "file-icons:apollo",
+        },
+        {
+          skillName: "Redis",
+          iconifyTag: "cib:redis",
+        },
+        {
+          skillName: "AWS",
+          iconifyTag: "skill-icons:aws-light",
+        },
+        {
+          skillName: "Jira",
+          iconifyTag: "simple-icons:jira",
+        },
+        {
+          skillName: "Git",
+          iconifyTag: "cib:git",
+        },
+        {
+          skillName: "Devops",
+          iconifyTag: "cib:azure-devops",
+        },
+        {
+          skillName: "Prisma",
+          iconifyTag: "simple-icons:prisma",
         },
         {
           skillName: "Github",
           iconifyTag: "akar-icons:github-fill",
+        },
+        {
+          skillName: "Gitlab",
+          iconifyTag: "logos:gitlab",
         },
         {
           skillName: "Docker",
@@ -90,24 +150,24 @@ export const skillsSection: SkillsSectionType = {
 
 export const SkillBars: SkillBarsType[] = [
   {
-    Stack: "Frontend/Design", //Insert stack or technology you have experience in
-    progressPercentage: "85", //Insert relative proficiency in percentage
+    Stack: "Smart Contract", //Insert stack or technology you have experience in
+    progressPercentage: "98", //Insert relative proficiency in percentage
   },
   {
-    Stack: "Backend",
+    Stack: "Web3",
     progressPercentage: "90",
   },
   {
-    Stack: "Programming",
+    Stack: "TheGraph",
     progressPercentage: "95",
   },
 ];
 
 export const educationInfo: EducationType[] = [
   {
-    schoolName: "University of Central Punjab",
-    subHeader: "Bachelor of Science in Computer Science",
-    duration: "September 2017 - April 2020",
+    schoolName: "Singapore Instituion of Technology",
+    subHeader: "Bachelor of  Computer Science",
+    duration: "September 2013 - April 2017",
     desc: "",
     grade: "Grade A",
     descBullets: [], // Array of Strings
@@ -116,45 +176,35 @@ export const educationInfo: EducationType[] = [
 
 export const experience: ExperienceType[] = [
   {
-    role: "Django Developer",
-    company: "Meganos Software",
-    companyLogo: "/img/icons/common/meganos.png",
-    date: "Aug 2022 - Present",
-    desc: "I crafted backends for diverse web apps, APIs, and WebSockets in e-commerce, podcasts, and property management. Managed server upkeep, deployments on Linux, Heroku, and AWS S3. Implemented PyTest for automated unit and integration testing, slashing 4 hours of manual testing each sprint, fortifying a dependable and flawless code foundation.",
+    role: "Fullstack Blockchain Developer",
+    company: "FKRPRO",
+    companyLogo: "/img/icons/common/FKRPRO.png",
+    date: "DEC 2022 - MAR 2024",
+    desc: "FKRPRO is a pioneering blockchain venture leveraging the Polygon network. Spearheaded a joint initiative with product and development teams, enhancing platform efficiency by 20%. Engaged in over 10 open source community projects, liaised with stakeholders, and provided mentorship and guidance to 3 emerging junior developers. Enhanced to engineering a robust backend infrastructure pivotal for the convergence of Web3 and FinTech.",
   },
   {
     role: "Full Stack Developer",
-    company: "Duseca Software",
-    companyLogo: "/img/icons/common/duseca_software_logo.jpeg",
-    date: "Jan 2022 - Sept 2023",
-    desc: "Crafted robust mobile application backends employing Django, Python, and REST APIs. Leveraged Django REST Framework, PostgreSQL, AWS, Firebase, Stripe, and WebSocket for efficient development. Collaborated across teams to surpass client expectations. Designed intuitive UIs, empowering clients to effortlessly manage their applications with a visually captivating interface.",
-    // descBullets: [
-    // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-    // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    // ],
+    company: "1PayrLink",
+    companyLogo: "/img/icons/common/PayrLink.jpg",
+    date: "DEC 2019 - NOV 2022",
+    desc: "Researched the requirements for a smart contract to be used in a cryptocurrency escrow project. Crafted the smart contract design for the escrow project, encompassing various functions and modules using UML diagrams. Implemented each stage of the ICO using Solidity and the Truffle framework to enhance project quality and accelerate development. Engaged in a collaborative effort with the Frontend and Web3 teams to optimize the efficiency of project development.",
   },
   {
-    role: "Full Stack Developer",
-    company: "ZRTechnologies",
-    companyLogo: "/img/icons/common/zrtech.jpeg",
-    date: "Aug 2022 - Jun 2023",
-    desc: "Crafting diverse web applications, I've elevated functionality and user engagement. Spearheaded Visual Portfolio, leveraging AI for captivating presentations from parsed text. Proficiently set up and optimized applications on Linux servers, guaranteeing smooth scalability and top-notch performance for seamless user experiences.",
+    role: "Web Developer",
+    company: "Wizardsdev",
+    companyLogo: "/img/icons/common/wizardsdev.png",
+    date: "OCT 2017 - DEC 2019",
+    desc: "WizardsDev is part of a large international holding company. It has offices in Boston, Tel Aviv, Baku, Dnepr, Kyiv, Kharkov, and other major cities. Implemented websites, mobile applications, and landing pages from concept through deployment. Conducted comprehensive assessments of UX and UI designs to ascertain technical feasibility, thereby optimizing the overall user experience and interface functionality. Enhanced on-chain data processing efficiency by a remarkable 12% through strategic deployment of optimized smart contract functions and implementing robust persistence storage strategies.",
   },
-  {
-    role: "Backend Developer",
-    company: "Bleed-AI",
-    companyLogo: "/img/icons/common/bleedAI.jpg",
-    date: "Sept 2021 - Oct 2021",
-    desc: "As a Django developer at Bleed AI, I integrated a computer vision AI model to process YouTube URLs on a website. I used Django Channels and Websockets to show real-time processing and implemented threading to run multiple instances of the AI model. I also used jQuery for UI rendering and deployed the website on a Heroku server.",
-  },
+  
 ];
 
 export const projects: ProjectType[] = [
   {
     name: "developer-portfolio",
-    desc: "Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.",
-    github: "https://github.com/1hanzla100/developer-portfolio",
-    link: "https://developer-portfolio-1hanzla100.vercel.app/",
+    desc: "Fullstack Blockchain Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a fullstack developer.",
+    github: "https://github.com/Superstar-Lucas",
+    link: "https://developer-portfolio-superstar-lucas.vercel.app/",
   },
   {
     name: "Giebo",
@@ -175,20 +225,20 @@ export const projects: ProjectType[] = [
 
 export const feedbacks: FeedbackType[] = [
   {
-    name: "Syed Jamal",
-    role: "Frontend Developer at Meganos Software",
+    name: "Daniel Shapero",
+    role: "CEO at FKRPRO",
     feedback:
-      "You were a wonderful addition to our team, alhumdulillah. I'm quite impressed with your abilities and professional practices. You approach problems from a unique perspective and always ask the right question. Finding a good teammate like Hanzla is extremely difficult. He has contributed to the growth of our organization and continues to do so. May Allah bless you with everything you desire.",
+      "You were a wonderful addition to our team, Lucas. I'm quite impressed with your abilities and professional practices. You approach problems from a unique perspective and always ask the right question. Finding a good teammate like Hanzla is extremely difficult. He has contributed to the growth of our organization and continues to do so.",
   },
   {
-    name: "Wajahat Malek",
-    role: "CEO at Duseca Software",
+    name: "Blake Lin",
+    role: "CIO at PayrLink",
     feedback:
-      "Hanzla has a deep understanding of web development technologies and is adept at using them to build scalable, robust, and secure web applications. He has worked on various projects, ranging from small-scale to large-scale, and has shown his ability to work collaboratively with others while maintaining a high level of productivity and quality. Hanzla is a pleasure to work with, and I have no doubt that he will be an asset to any team or organization. I highly recommend Hanzla for any Full Stack Django, React.js, or Blockchain development projects, and I wish him all the best in his future endeavors.",
+      "Lucas has a deep understanding of web development technologies and is adept at using them to build scalable, robust, and secure web applications. He has worked on various projects, ranging from small-scale to large-scale, and has shown his ability to work collaboratively with others while maintaining a high level of productivity and quality. Lucas is a pleasure to work with, and I have no doubt that he will be an asset to any team or organization. I highly recommend Lucas for any Full Stack Blockchain development projects, and I wish him all the best in his future endeavors.",
   },
   {
-    name: "Zaid Zaffar",
-    role: "CEO at ZR Technologies",
+    name: "Vadym Gontsov",
+    role: "CEO at Wizardsdev",
     feedback:
       "Very skilled at what he does and has an in depth knowledge of Django and he's always ready to handle any challenges thrown at him. He's been a valuable member of my team and i look forward to working with him in the years to come",
   },
@@ -196,10 +246,10 @@ export const feedbacks: FeedbackType[] = [
 
 // See object prototype on /types/section.ts page
 export const seoData: SEODataType = {
-  title: "Hanzla Tauqeer",
+  title: "Lucas Lee",
   description: greetings.description,
-  author: "Hanzla Tauqeer",
-  image: "https://avatars.githubusercontent.com/u/59178380?v=4",
+  author: "Lucas Lee",
+  image: "https://avatars.githubusercontent.com/u/59178380?v=4https://avatars.githubusercontent.com/u/9166113?v=4",
   url: "https://developer-portfolio-1hanzla100.vercel.app",
   keywords: [
     "Hanzla",
